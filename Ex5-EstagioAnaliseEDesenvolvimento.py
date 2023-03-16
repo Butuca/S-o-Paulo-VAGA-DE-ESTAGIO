ad0ae4b0b4bc00b80b0526757c6c03bb9e3a40a6
Lucas Santana Saturnino <lucasssaturnino@hotmail.com>
2023-03-16 19:53:08 -0300

    Update Ex5-EstagioAnaliseEDesenvolvimento.py

--- Ex5-EstagioAnaliseEDesenvolvimento.py
+++ Ex5-EstagioAnaliseEDesenvolvimento.py
@@ -1,7 +1,9 @@
 string = 'Ola Mundo'
 stringInvertida = ''
 
+#Percorre a String de trás para frente
 for i in range(len(string)-1,-1,-1):
+    #atribui os caracteres para a nova String
     stringInvertida += string[i]
 
 print(stringInvertida)
